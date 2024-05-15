2e7b3e6573ae29916b6fa34c0ed822e569fe1cb2
Isaac4Lion <92281039+Isaac4Lion@users.noreply.github.com>
2024-05-15 08:42:47 -0500

    Cambios en la presentacion de las frases, y modificacion en su estilo. Adicional, un boton para ir a la página de busqueda.

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,15 @@
 // Espera a que el documento esté completamente cargado
 $(document).ready(function() {
     // Selecciona el botón por su ID
-    var btnRedireccion = document.querySelectorAll(".boton");
-    var popUp = document.getElementById("popUp");
-    var overlay = document.getElementById("overlay");
-    var tituloPopUp = document.getElementById("tituloPopUp");
-    var cerrarPopUp = document.getElementById("cerrarPopUp");
+    let btnRedireccion = document.querySelectorAll(".boton");
+    const randomBtn = document.getElementById("randomBtn");
+    let popUp = document.getElementById("popUp");
+    let overlay = document.getElementById("overlay");
+    let tituloPopUp = document.getElementById("tituloPopUp");
+    let cerrarPopUp = document.getElementById("cerrarPopUp");
 
-    var textoID= document.getElementById("textoID");
     var textoURL= document.getElementById("textoURL");
     var textoValor= document.getElementById("textoValor");
-    var textoImagen= document.getElementById("textoImagen");
     var textoCreado= document.getElementById("textoFC");
     var textoActualizado= document.getElementById("textoFA");
 
@@ -18,32 +17,36 @@ $(document).ready(function() {
     btnRedireccion.forEach(function(boton) {
         // Añadir a cada boton el evento onClick
         boton.addEventListener('click', function() {
+            const categoria = boton.textContent.trim().toLowerCase();
             // Obtiene la categoría del texto del botón
-            var categoria = boton.textContent.trim();
             
             // Si se proporciona una categoría, hace la solicitud a la API
             if (categoria) {
-                fetch(`https://api.chucknorris.io/jokes/random?category=${categoria}`)
+                obtenerData(categoria);
+                randomBtn.addEventListener('click',()=>obtenerData(categoria));
+            }
+        });
+    });
+
+const obtenerData = (categoria) =>{
+    fetch(`https://api.chucknorris.io/jokes/random?category=${categoria}`)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
                     // Configura el contenido del pop-up
-                    tituloPopUp.innerHTML = "<strong>Categoria:</strong> " + categoria;
-                    textoID.innerHTML = "<strong>ID:</strong> " + data.id;
-                    textoURL.innerHTML = "<strong>URL:</strong> " + data.url;
-                    textoValor.innerHTML = "<strong>Valor:</strong> " + data.value;
-                    textoImagen.innerHTML = "<strong>Imagen:</strong> " + data.icon_url;
-                    textoCreado.innerHTML = "<strong>Creado en:</strong> " + data.created_at;
-                    textoActualizado.innerHTML = "<strong>Actualizado en:</strong> " + data.updated_at; // Agregar el valor al Pop-Up
+                    tituloPopUp.innerHTML = "<strong>Category:</strong> " + categoria.toUpperCase();
+                    textoValor.innerHTML = `<p class="joke">"${data.value}"</p> `;
+                    textoCreado.innerHTML = "<strong>Created at:</strong> " + data.created_at.substr(0,19);
+                    textoActualizado.innerHTML = "<strong>Updated at:</strong> " + data.updated_at.substr(0,19); // Agregar el valor al Pop-Up
+                    textoURL.innerHTML = `<a href=${data.url} target="_blank">See more...</a> `;
                     
                     // Muestra el pop-up
                     popUp.style.display = "block";
                     overlay.style.display = "block";
                 })
                 .catch(error => console.log(error));
-            }
-        });
-    });
+    textoValor.innerHTML = '<p class="text-center">...</p>'
+}
 
     cerrarPopUp.addEventListener('click', function() {
         // Oculta el pop-up al hacer clic en el botón de cerrar
